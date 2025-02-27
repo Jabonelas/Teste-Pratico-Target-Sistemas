29c5295ea9df74f601b360ed36d100a22562ce08
israel <israel094@hotmail.com>
2025-02-26 22:20:33 -0300

    Correcao para implementacao do dados do arquivo json, pois tinha feito sem verificar o email com os arquivos

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace ConsoleApp2
+﻿using System.Text.Json;
+
+namespace ConsoleApp2
 {
     internal class Program
     {
@@ -9,6 +11,24 @@
             //Resposta o valor da SOMA sera: 91
 
             //Questao 2
+
+            Questao2();
+
+            //Questao 3
+
+            Questao3();
+
+            //Questao 4
+
+            Questao4();
+
+            //Questao 5
+
+            Questao5();
+        }
+
+        public static void Questao2()
+        {
             Console.WriteLine("Questao 2 \n");
 
             Console.Write("informe o valor:");
@@ -29,30 +49,151 @@
 
                 b = valorSoma;
             }
+        }
 
-            //Questao 3
+        public static void Questao3()
+        {
             Console.WriteLine("\n\nQuestao 3 \n");
 
-            double[] faturamentoDiario = {
-                120.50,  95.30,  80.00,  150.75,  110.20,  0,  0,
-                140.10, 85.90,  100.00,  75.80,   95.00,   0,  0,
-                60.30,  145.20, 110.40,  50.00,   140.00,  0,  0,
-                170.30,  95.70,  105.20,  90.00,   115.50,  0,  0,
-                75.90,  95.20
-            };
+            string json = @"[
+	{
+		""dia"": 1,
+		""valor"": 22174.1664
+	},
+	{
+		""dia"": 2,
+		""valor"": 24537.6698
+	},
+	{
+		""dia"": 3,
+		""valor"": 26139.6134
+	},
+	{
+		""dia"": 4,
+		""valor"": 0.0
+	},
+	{
+		""dia"": 5,
+		""valor"": 0.0
+	},
+	{
+		""dia"": 6,
+		""valor"": 26742.6612
+	},
+	{
+		""dia"": 7,
+		""valor"": 0.0
+	},
+	{
+		""dia"": 8,
+		""valor"": 42889.2258
+	},
+	{
+		""dia"": 9,
+		""valor"": 46251.174
+	},
+	{
+		""dia"": 10,
+		""valor"": 11191.4722
+	},
+	{
+		""dia"": 11,
+		""valor"": 0.0
+	},
+	{
+		""dia"": 12,
+		""valor"": 0.0
+	},
+	{
+		""dia"": 13,
+		""valor"": 3847.4823
+	},
+	{
+		""dia"": 14,
+		""valor"": 373.7838
+	},
+	{
+		""dia"": 15,
+		""valor"": 2659.7563
+	},
+	{
+		""dia"": 16,
+		""valor"": 48924.2448
+	},
+	{
+		""dia"": 17,
+		""valor"": 18419.2614
+	},
+	{
+		""dia"": 18,
+		""valor"": 0.0
+	},
+	{
+		""dia"": 19,
+		""valor"": 0.0
+	},
+	{
+		""dia"": 20,
+		""valor"": 35240.1826
+	},
+	{
+		""dia"": 21,
+		""valor"": 43829.1667
+	},
+	{
+		""dia"": 22,
+		""valor"": 18235.6852
+	},
+	{
+		""dia"": 23,
+		""valor"": 4355.0662
+	},
+	{
+		""dia"": 24,
+		""valor"": 13327.1025
+	},
+	{
+		""dia"": 25,
+		""valor"": 0.0
+	},
+	{
+		""dia"": 26,
+		""valor"": 0.0
+	},
+	{
+		""dia"": 27,
+		""valor"": 25681.8318
+	},
+	{
+		""dia"": 28,
+		""valor"": 1718.1221
+	},
+	{
+		""dia"": 29,
+		""valor"": 13220.495
+	},
+	{
+		""dia"": 30,
+		""valor"": 8414.61
+	}
+]";
 
-            double mediaMensal = faturamentoDiario.Where(x => x != 0).Average();
+            List<Faturamento> faturamentoDiario = JsonSerializer.Deserialize<List<Faturamento>>(json);
 
-            double qtdDias = faturamentoDiario.Where(x => x >= mediaMensal).Count();
+            double mediaMensal = faturamentoDiario.Where(x => x.Valor != 0).Average(x => x.Valor);
 
-            var menorValor = faturamentoDiario.Where(x => x > 0).Min();
+            double qtdDias = faturamentoDiario.Where(x => x.Valor >= mediaMensal).Count();
+
+            var menorValor = faturamentoDiario.Where(x => x.Valor > 0).Min();
             var maiorValor = faturamentoDiario.Max();
 
             Console.WriteLine($"Menor valor: {menorValor}");
             Console.WriteLine($"Maior valor: {maiorValor}");
             Console.WriteLine($"Quantidade de dias em que o faturamento foi maior que a media: {qtdDias}");
+        }
 
-            //Questao 4
+        public static void Questao4()
+        {
             Console.WriteLine("\nQuestao 4 \n");
 
             var faturamento = new Dictionary<string, double>()
@@ -72,8 +213,10 @@
 
                 Console.WriteLine($"{item.Key}, Faturamento: {item.Value}, Porcentagem:{percentual.ToString("N")}%");
             }
+        }
 
-            //Questao 5
+        public static void Questao5()
+        {
             Console.WriteLine("\nQuestao 5 \n");
 
             string palavra = "Teste";
@@ -95,5 +238,11 @@
 
             Console.WriteLine($"Palavra normal: {palavra} \nPalavra invertida: {palavraInvertida}");
         }
+    }
+
+    public class Faturamento
+    {
+        public int Dia { get; set; }
+        public double Valor { get; set; }
     }
 }
